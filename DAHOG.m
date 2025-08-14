5cc1af5f98aef5467e1897a953dff2cf8028e0e4
MaryamMajidi <155631285+MaryamMajidi93@users.noreply.github.com>
2025-08-14 17:25:16 +0330

    Update DAHOG.m

--- DAHOG.m
+++ DAHOG.m
@@ -1,10 +1,25 @@
 function  [W,H] = DAHOG(X,XX,XXX,DH,DW,SH,SW,alpha,beta,lambda,k,m,n,maxiter)
+
+%%%%%%%%%%%%%%%%%%%%
+%% X: Data set in R_+^(m*n),  where m and n are the numbers of samples (words) and features (documents), respectively.
+%% SW: Similarity matrix in R_+^(m*m) associated with the data samples (words).
+%% DW: Degree matrix in R_+^(m*m) obtained from SW.
+%% SH: Similarity matrix in R_+^(n*n) associated with the data features (documents).
+%% DH: Degree matrix in R_+^(n*n) obtained from SH.
+%% alpha, beta, lambda: Regularization parameters.
+%% k: The number of topics.
+%% m: The numbers of samples (words)
+%% n: The numbers of features (documents)
+%% maxiter: Maximum number of iterations
+
+
+
 %% Initialization
 W = rand(m,k);
 H = rand(k,n);
 iter=1;
 while iter<=maxiter
-    %% Update H
+    %% Update the topic-document matrix H
     XXH = H*XX;
     WX = (W')*X;
     numH = 3*WX + 2*H*(WX')*WX + alpha*H*SH + lambda*H;
@@ -12,7 +27,8 @@ while iter<=maxiter
     reH = numH./max(denH,1e-10);
     reH = nthroot(reH,4);
     H = H.*reH;
-    %% Update W
+    
+    %% Update the word-topic matrix W
     XH = X*(H');
     XXW = XXX*W;
     numW = 3*X*(H') + 2*XH*(XH')*W + beta*SW*W;
