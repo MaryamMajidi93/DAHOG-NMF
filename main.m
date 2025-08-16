6fed394ca84db39234b63399f05c6f7d831363cc
MaryamMajidi <155631285+MaryamMajidi93@users.noreply.github.com>
2025-08-16 06:49:23 +0330

    Update main.m

--- main.m
+++ main.m
@@ -20,141 +20,26 @@
 
 
 %% Outputs
-%% H: Topic-Document Matrix.
-%% W: Word-topic matrix. 
+%% V: Topic-Document Matrix.
+%% U: Word-topic matrix. 
 clc
 clear
 close all
 format shortG
 addpath('./AdditionalFiles');
 
-runNMI=zeros(50,1);
-runACC=zeros(50,1);
-runCoh=zeros(50,1);
-runARI=zeros(50,1);
 
-alpha = [1e-08, 1e-06, 1e-04, 1e-02, 1, 1e02, 1e04, 1e06, 1e08, 1e10, 1e12];
+alpha = [1e-08, 1e-06, 1e-04, 1e-02, 1, 1e02, 1e04, 1e06, 1e08, 1e10, 1e12] %% This parameter needs to be tuned.
+lambda = [1e-08, 1e-06, 1e-04, 1e-02, 1, 1e02, 1e04, 1e06, 1e08, 1e10, 1e12]; %% This parameter needs to be tuned.
+kmanifold = [5, 10, 15, 20]; %% This parameter needs to be tuned.
+k =[3:10, 15, 20, 25]; %% The number of topics.
+maxiter = 100;
 
-lambda = [1e-08, 1e-06, 1e-04, 1e-02, 1, 1e02, 1e04, 1e06, 1e08, 1e10, 1e12];
-
-kmanifold = [5, 10]; 
-
-
-NTopics =[3:10, 15, 20, 25];
-
-ACCmean  =  zeros(length(alpha),length(lambda),length(kmanifold));
-NMImean  =  zeros(length(alpha),length(lambda),length(kmanifold));
-Cohmean  =  zeros(length(alpha),length(lambda),length(kmanifold));
-ARImean  = zeros(length(alpha),length(lambda),length(kmanifold));
-
-
-%% Load Dataset
-%load('/home/392030/TopicNMF/OurDualAutoTDT2/TDT2/TDT2.mat')
-load('TDT2\TDT2.mat')
-
-for NT=NTopics
-    for i=1:length(alpha) %% Grid Search for alpha
-        for ii=1:length(lambda)  %% Grid Search for lambda
-            for iii=1:length(kmanifold) %% Grid Search for kmanifold
-                for run = 1:50
-                    disp(run)
-                    SS =[num2str(run) '.mat'];
-                    %SS = ['/home/392030/TopicNMF/OurDualAutoTDT2/TDT2/' num2str(NT) 'Class/' SS];
-                    SS = ['TDT2\' num2str(NT) 'Class\' SS];
-                    load(SS);
-                    fea2 = fea(sampleIdx,:);
-                    gnd2 = gnd(sampleIdx,:);
-
-                    fea2(:,zeroIdx) = [];
-
-                    II = unique(gnd2);
-                    for kk = 1:length(II)
-                        for l = 1:length(gnd2)
-                            if gnd2(l)==II(kk)
-                                gnd2(l) = kk;
-                            end
-                        end
-                    end
-
-                    X = NormalizeFea(fea2,1); % fea2 id doc-word matrix
-                    X = X'; % final X will be word-doc matrix
-                    %% Please not that you should put the data samples in the columns of data
-                    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                    %% Parameters
-                    XX = (X')*X;
-                    XXX = X * (X');
-                    [m,n] = size(X);
-                    maxiter = 200;
-                    c =  length(unique(gnd2));
-                    k = NT;
-                    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                    %% OurDualAuto method
-
-                    [DH,SH] = makeLaplacian(X', kmanifold(iii),alpha(i),lambda(ii));
-                    [DW,SW] = makeLaplacian(X, kmanifold(iii),alpha(i),lambda(ii));
-                    [U,V] = OurDualAuto(X,XX,XXX,DH,DW,SH,SW,alpha(i),alpha(i),lambda(ii),k,m,n,maxiter);
-                    %% Clustering
-                    tempNMI=zeros(20,1);
-                    tempACC=zeros(20,1);
-                    tempCoh=zeros(20,1);
-                    tempARI=zeros(20,1);
-                    for j = 1:20
-                        IDX = kmeans(V',c); %%kmeans  V is topic-doc matrix
-                        tempNMI(j) = nmi(gnd2,IDX);   %% NMI
-                        tempACC(j) = clusterAccMea(gnd2,IDX); %% ACC
-                        tempCoh(j) = coherence_3(U,X,25); %% Coh   (U is word-topic matrix)
-                        tempARI(j) = randindex(gnd2, IDX); %% ARI
-                    end
-                    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                    runACC(run) = mean(tempACC);
-                    runNMI(run) = mean(tempNMI);
-                    runCoh(run) = mean(tempCoh);
-                    runARI(run) = mean(tempARI);
-                    disp(runARI(run));
-                    
-                end
-                ACCmean(i,ii,iii) = mean(runACC);
-                NMImean(i,ii,iii) = mean(runNMI);
-                Cohmean(i,ii,iii) = mean(runCoh);
-                ARImean(i,ii,iii) = mean(runARI);
-                
-                meanMats=[ACCmean;NMImean;Cohmean;ARImean];
-                Mats =['save meanMats_' num2str(NT) '_Topic   meanMats;'];
-                eval(Mats);
-                
-                fprintf(1,'# of Topics = %d ,mean for alpha%d & lambda%d & kmanifold%d \n',NT,i,ii,iii);
-                disp('****************************************')
-                disp('ACCmean =')
-                disp(ACCmean(i,ii,iii))
-                disp('NMImean =')
-                disp(NMImean(i,ii,iii))
-                disp('Cohmean =')
-                disp(Cohmean(i,ii,iii))
-                disp('ARImean =')
-                disp(ARImean(i,ii,iii))
-            end
-        end
-    end
-    
-    ACCfinalmean=max(ACCmean(:));
-    NMIfinalmean=max(NMImean(:));
-    Cohfinalmean=max(Cohmean(:));
-    ARIfinalmean=max(ARImean(:));
-
-    finalResults = [ACCfinalmean;NMIfinalmean;Cohfinalmean;ARIfinalmean];
-    % xlswrite('myResults.xlsx',finalResults,'ONMF-H','D18:D21')
-    SS2 =['save Finalresults_' num2str(NT) '_Topic   finalResults;'];
-    eval(SS2);
-    fprintf(1,'For #Tpoics = %d , The final results are: \n',NT)
-    disp('finalACC =')
-    disp(ACCfinalmean)
-    disp('finalNMI =')
-    disp(NMIfinalmean)
-    disp('finalCoh =')
-    disp(Cohfinalmean)
-    disp('finalARI =')
-    disp(ARIfinalmean)
-    disp('****************************************')
-
-end
-
+%% load data contaning fea and gnd
+X = NormalizeFea(fea,1); % fea is document-word matrix
+X = X';
+%% Please not that you should put the data samples in the columns of data and the final X will be word-document matrix
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[DH,SH] = makeLaplacian(X', kmanifold,alpha,lambda);
+[DW,SW] = makeLaplacian(X, kmanifold,alpha,lambda);
+[U,V] = OurDualAuto(X,DH,DW,SH,SW,alpha,alpha,lambda,k,maxiter);
